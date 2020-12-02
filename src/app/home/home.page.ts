90b92b4fe826c004ae348f7d90fe323009e93016
takeuchw <toshiowillian@gmail.com>
2020-12-01 22:11:07 -0300

    add a rota dos patchnotes na home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  constructor(public toastController: ToastController, private router: Router) {}
+
+  async ngOnInit() {
+    const toast = await this.toastController.create({
+      header: '',
+      message: 'Veja nossos patchnotes!',
+      position: 'bottom',
+      buttons: [
+        {
+          text: 'Clique aqui',
+          role: 'cancel',
+          handler: () => {
+            toast.dismiss();
+            this.router.navigate(['/patchnotes']);
+          }
+        }
+      ]
+    });
+    toast.present();
+  }
 
 }
